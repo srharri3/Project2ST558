db8d314503cd09e66e006f7c9d0487471aa892ed
Savannah Harrison <srharri3@ncsu.edu>
2024-11-06 15:39:49 -0500

    saving changes

--- User_Behavior_App/app.R
+++ User_Behavior_App/app.R
@@ -45,7 +45,8 @@ ui <- dashboardPage(
               sidebarLayout(
                 sidebarPanel(
                   h2("Select a subset of the data:"),
-                  radioButtons(
+                 
+                   radioButtons(
                     inputId = "device_model",
                     label = "Device Model",
                     choices = c("Google Pixel 5", "OnePlus 9", "Xiaomi Mi 11", "iPhone 12", "Samsung Galaxy S21")
@@ -64,24 +65,22 @@ ui <- dashboardPage(
                   ),
                   
                   h2("Select Numeric Variables"),
+                  
                   selectizeInput(
                     inputId = "numvar1",
                     label = "Numeric Variable 1",
-                    choices = c("Device.Model", "App.Usage.Time..min.day.","Screen.On.Time..hours.day.", "Battery.Drain..mAh.day.", "Number.of.Apps.Installed", "Data.Usage..MB.day.", "User.Behavior.Class")
+                    choices = c("User.ID", "App.Usage.Time..min.day.","Screen.On.Time..hours.day.", "Battery.Drain..mAh.day.", "Number.of.Apps.Installed", "Data.Usage..MB.day.", "User.Behavior.Class")
                   ),
                   
                   selectizeInput(
                     inputId = "numvar2",
                     label = "Numeric Variable 2",
-                    choices = c("Device.Model", "App.Usage.Time..min.day.","Screen.On.Time..hours.day.", "Battery.Drain..mAh.day.", "Number.of.Apps.Installed", "Data.Usage..MB.day.", "User.Behavior.Class",
-                    selected = "Device.Model"
+                    choices = c("User.ID", "App.Usage.Time..min.day.","Screen.On.Time..hours.day.", "Battery.Drain..mAh.day.", "Number.of.Apps.Installed", "Data.Usage..MB.day.", "User.Behavior.Class",
+                    selected = "App.Usage.Time..min.day."
                   ),
                   # Action Button to apply the filters
                   actionButton("apply", "Apply Filters")
                 ),
-                
-                sliderInput("slidey", "Variable Slider", min = 0, max = 10, value = 5),
-              
                 
                 mainPanel(
                   DT::dataTableOutput("data_table"),
@@ -92,32 +91,44 @@ ui <- dashboardPage(
       
       tabItem(tabName = "dataexplore",
               titlePanel("Data Exploration"),
-              sidebarLayout(
-                sidebarPanel(
-                  selectizeInput(
-                    inputId = "num",
-                    label = "x Variable",
-                    choices = numeric_vars,
-                    selected = "PINCP"
-                  ),
-                  
-                  selectizeInput(
-                    inputId = "corr_y",
-                    label = "y Variable",
-                    choices = numeric_vars
-                  ),
-                  
+              h2("Select a subset of the data:"),
+              
+              radioButtons(
+                inputId = "device_model",
+                label = "Device Model",
+                choices = c("Google Pixel 5", "OnePlus 9", "Xiaomi Mi 11", "iPhone 12", "Samsung Galaxy S21")
+              ),
+              
+              radioButtons(
+                inputId = "op_sys",
+                label = "Operating System",
+                choices = c("Android", "iOS")
+              ),
+              
+              radioButtons(
+                inputId = "gender",
+                label = "Gender",
+                choices = c("Male", "Female")
+              ),
+              
+              h2("Select Numeric Variables"),
+              
+              selectizeInput(
+                inputId = "numvar1",
+                label = "Numeric Variable 1",
+                choices = c("User.ID", "App.Usage.Time..min.day.","Screen.On.Time..hours.day.", "Battery.Drain..mAh.day.", "Number.of.Apps.Installed", "Data.Usage..MB.day.", "User.Behavior.Class")
+              ),
+              
+              selectizeInput(
+                inputId = "numvar2",
+                label = "Numeric Variable 2",
+                choices = c("User.ID", "App.Usage.Time..min.day.","Screen.On.Time..hours.day.", "Battery.Drain..mAh.day.", "Number.of.Apps.Installed", "Data.Usage..MB.day.", "User.Behavior.Class",
+                            selected = "App.Usage.Time..min.day."
                 ),
-                mainPanel(
-                  tabsetPanel(
-                    tabPanel("Summary Statistics", verbatimTextOutput("summary_stats")),
-                    tabPanel("Plot", plotOutput("summary_plot"))
-                  )
-                )
-              )
-      )
-),
-
+                # Action Button to apply the filters
+                actionButton("apply", "Apply Filters")
+              ),
+            
     
 server <- function(input, output, session) {
   
@@ -150,18 +161,18 @@ server <- function(input, output, session) {
   })
   
   output$slider_ui_1 <- renderUI({
-    req(input$numeric_var_1)
+    req(input$numeric_)
     data <- user_data()
-    range_vals <- range(data[[input$numeric_var_1]], na.rm = TRUE)
+    range_vals <- range(data[[input$numvar1]], na.rm = TRUE)
     sliderInput("numeric_range_1", "Select Range for Numeric Variable 1:", 
                 min = range_vals[1], max = range_vals[2], 
                 value = range_vals, step = 1)
   })
   
   output$slider_ui_2 <- renderUI({
-    req(input$numeric_var_2)
+    req(input$numvar2)
     data <- user_data()
-    range_vals <- range(data[[input$numeric_var_2]], na.rm = TRUE)
+    range_vals <- range(data[[input$numvar2]], na.rm = TRUE)
     sliderInput("numeric_range_2", "Select Range for Numeric Variable 2:", 
                 min = range_vals[1], max = range_vals[2], 
                 value = range_vals, step = 1)
@@ -183,8 +194,6 @@ server <- function(input, output, session) {
     })
   })
   
-    
-    # Render numeric summaries
     output$summary_stats <- renderPrint({
       summary(data[[input$summary_var]])
     })
